aebd67b6e3e74629bfaf7b0476e5d4498bfb733d
Vikicaa <viktor2xx1@gmail.com>
2023-06-26 18:03:42 +0200

    email send design

--- forgotpwsite.php
+++ forgotpwsite.php
@@ -41,7 +41,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['errors'] = $errors;
         }
     }
-} else {
+    header('Location: login.php');
+} else {   
     unset($_SESSION['errors']);
 }
 $connection->close();
