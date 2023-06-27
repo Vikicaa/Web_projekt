9c9830de03250284d7ec24d049f6a61ca85052d7
Vikicaa <viktor2xx1@gmail.com>
2023-06-27 17:37:41 +0200

    db config

--- db_config1.php
+++ db_config1.php
@@ -1,25 +1,5 @@
 <?php
-/*
-    define("HOST","localhost");
-    define("USER","bw");
-    define("PASSWORD","4qEA1dED43ObX44");
-    define("DATABASE","bw");
 
-    $connection = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
-    if (mysqli_connect_errno())
-    {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-
-    mysqli_query($connection,"SET NAMES utf8") or die (mysqli_error($connection));
-    mysqli_query($connection,"SET CHARACTER SET utf8") or die (mysqli_error($connection));
-    mysqli_query($connection,"SET COLLATION_CONNECTION='utf8_general_ci'") or die (mysqli_error($connection));
-
-define("HOST", "localhost");
-define("USER", "bw");
-define("PASSWORD", "4qEA1dED43ObX44");
-define("DATABASE", "bw");
-*/
 $host = 'localhost'; // adatbázis szerver elérési útja
 $username = 'root'; // adatbázis felhasználónév
 $password = ''; // adatbázis jelszó
